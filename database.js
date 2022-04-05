74f4cfc0261bc327de8a0605aa052393b01c14a5
Yulim Kim <yulimkim@email.unc.edu>
2022-04-05 14:39:43 -0400

    re figure out help and database

---
+++ database.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const database = require("better-sqlite3")
+const db = new database("log.db")
+
+// Is the database initialized or do we need to initialize it?
+const stmt = db.prepare(`
+    SELECT name FROM sqlite_master WHERE type='table' and name='userinfo';`
+    );
+    
+// Define row using `get()` from better-sqlite3
+let row = stmt.get();
+// Check if there is a table. If row is undefined then no table exists.
+if (row === undefined) {
+// Echo information about what you are doing to the console.
+    console.log('Your database appears to be empty. I will initialize it now.');
+// Set a const that will contain your SQL commands to initialize the database.
+    const sqlInit = `
+        CREATE TABLE userinfo ( id INTEGER PRIMARY KEY, username TEXT, password TEXT );
+        INSERT INTO userinfo (username, password) VALUES ('user1','supersecurepassword'),('test','anotherpassword');
+    `;
+// Execute SQL commands that we just wrote above.
+    db.exec(sqlInit);
+// Echo information about what we just did to the console.
+    console.log('Your database has been initialized with a new table and two entries containing a username and password.');
+} else {
+// Since the database already exists, echo that to the console.
+    console.log('Database exists.')
+}
+// Export all of the above as a module so that we can use it elsewhere.
+module.exports = db
