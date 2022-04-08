eeba7f8f1d4f1be8da3c8be63a698ae26f8b026e
Yulim Kim <yulimkim@email.unc.edu>
2022-04-08 17:19:39 -0400

    add functions for endpoints?

--- server.js
+++ server.js
@@ -65,6 +65,51 @@ let logdata = {
     next();
 });
 
+//flip functions 
+function coinFlip() {
+    return (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails';
+  }
+
+function coinFlips(flips) {
+    let f = [];
+    for (var i = 0; i < flips; i++) {
+      let flip = coinFlip();
+      f[i] = flip;
+    }
+    return f;
+  }
+
+function countFlips(array) {
+    let heads_count = 0;
+    let tails_count = 0;
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] == 'heads') {
+        heads_count ++;
+      } else if (array[i] == 'tails') {
+        tails_count ++;
+      }
+    }
+    if (heads_count == 0) {
+      return {"tails": tails_count};
+    }
+    else if (tails_count == 0) {
+      return {"heads": heads_count};
+    }
+    return {"heads": heads_count, "tails": tails_count};
+  }
+  
+function flipACoin(call) {
+    let coin_flip = coinFlip();
+    let match = "";
+    if (coin_flip == call) {
+      match = "win";
+    } else if (coin_flip != call) {
+      match = "lose";
+    }
+    return {"call": call, "flip": coin_flip, "result": match};
+  }
+
+// endpoints
 app.get('/app/', (req, res) => {
     res.statusMessage = 'OK';
     res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
