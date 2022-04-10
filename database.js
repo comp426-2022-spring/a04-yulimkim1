3b14f5786ce499f50826a6714608dce514993aca
Yulim Kim <yulimkim@email.unc.edu>
2022-04-10 14:09:32 -0400

    access log table fix

--- database.js
+++ database.js
@@ -8,9 +8,19 @@ const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and n
 let row = stmt.get();
 
 if (row === undefined) {
-    const sqlInit = `CREATE TABLE accesslog ( id INTEGER NOT NULL PRIMARY KEY,
-         remoteaddr TEXT, remoteuser TEXT, time INTEGER, method TEXT, url TEXT,
-          protocol TEXT, httpversion TEXT, secure TEXT, status TEXT, referer TEXT, useragent TEXT);`;
+    const sqlInit = `CREATE TABLE accesslog (
+        id INTEGER PRIMARY KEY, 
+        remoteaddr VARCHAR, 
+        remoteuser VARCHAR, 
+        time VARCHAR, 
+        method VARCHAR, 
+        url VARCHAR, 
+        protocol VARCHAR,
+        httpversion NUMERIC, 
+        status INTEGER, 
+        referer VARCHAR,
+        useragent VARCHAR
+    );`;
     db.exec(sqlInit);
 } else {
     console.log("Database exists");
--- server.js
+++ server.js
@@ -55,13 +55,15 @@ let logdata = {
         url: req.url,
         protocol: req.protocol,
         httpversion: req.httpVersion,
-        secure: req.secure,
+        //secure: req.secure,
         status: res.statusCode,
         referer: req.headers['referer'],
         useragent: req.headers['user-agent']
     }
-    const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, secure, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
-    const table_info = stmt.run(String(logdata.remoteaddr), String(logdata.remoteuser), String(logdata.time), String(logdata.method), String(logdata.url), String(logdata.protocol), String(logdata.httpversion), String(logdata.secure), String(logdata.status), String(logdata.referer), String(logdata.useragent))
+    const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+    //const table_info = stmt.run(String(logdata.remoteaddr), String(logdata.remoteuser), String(logdata.time), String(logdata.method), String(logdata.url), String(logdata.protocol), String(logdata.httpversion), String(logdata.status), String(logdata.referer), String(logdata.useragent))
+    const table_info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
+
     next();
 });
 
